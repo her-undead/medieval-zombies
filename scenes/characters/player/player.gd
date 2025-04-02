75e286371908054de26fbe1cc2c2b36af510f738
jkatz1234 <108993856+jkatz1234@users.noreply.github.com>
2025-04-02 13:21:25 -0400

    Can't move while dialogue active

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -6,6 +6,15 @@ const SPEED = 300.0
 
 @export var inventory: Inventory
 
+var is_chatting = false
+var player_in_chatzone = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("chat") && player_in_chatzone:
+		is_chatting = true
+	#print(player_in_chatzone)
+	#print(is_chatting)
+
 func _physics_process(delta: float) -> void:
 	
 	# Add the gravity.
@@ -20,16 +29,28 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if (!is_chatting):
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	var directionY := Input.get_axis("ui_up", "ui_down")
-	if directionY:
-		velocity.y = directionY * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		var directionY := Input.get_axis("ui_up", "ui_down")
+		if directionY:
+			velocity.y = directionY * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Villagers"):
+		player_in_chatzone = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Villagers"):
+		player_in_chatzone = false
+		
