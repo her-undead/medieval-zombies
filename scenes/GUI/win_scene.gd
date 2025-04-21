a6095ae0a2215d4a5ee33a6f215a7a8de60c8a2d
audreycharge <64012818+audreycharge@users.noreply.github.com>
2025-04-20 20:19:58 -0400

    made dialog opaque and added epilogue

---
+++ scenes/GUI/win_scene.gd
@@ -0,0 +1,18 @@
+extends Control
+class_name WinScene
+
+@onready var replay_button = $ReplayButton
+@onready var exit_button = $ReturnButton
+@onready var start_game = preload("res://scenes/locations/home.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	replay_button.button_down.connect(on_replay_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+func on_replay_pressed() -> void:
+	get_tree().call_deferred('change_scene_to_file', "res://scenes/locations/home.tscn")
+
+func on_exit_pressed() -> void:
+	get_tree().call_deferred('change_scene_to_file', "res://scenes/GUI/start_menu.tscn")
